2ff31549907cee923c9a866183a73899ab50d10d
MitulMistry <mitul.mistry1@gmail.com>
2015-11-29 00:21:24 +0000

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,16 @@
+require 'prime'
+
 # Enter your procedural solution here!
+def largest_prime_factor(num)
+  #prime_division returns an array of 2 element arrays containing prime factorization - [[2, 1], [5, 2]]
+  #flat_map contatenates the 2 element arrays into a single array [2, 5, 5]
+  array = Prime.prime_division(num).flat_map { |factor, power| [factor] * power }
+
+  largest = 0
+  array.each do |i|
+    if i > largest
+      largest = i
+    end
+  end
+  return largest
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,23 @@
 # Enter your object-oriented solution here!
+require 'prime'
+
+class LargestPrimeFactor
+  def initialize(num)
+    @num = num
+  end
+
+  def number
+    #prime_division returns an array of 2 element arrays containing prime factorization - [[2, 1], [5, 2]]
+    #flat_map contatenates the 2 element arrays into a single array [2, 5, 5]
+    array = Prime.prime_division(@num).flat_map { |factor, power| [factor] * power }
+
+    largest = 0
+    array.each do |i|
+      if i > largest
+        largest = i
+      end
+    end
+    return largest
+  end
+
+end
